325ac15ea16df48dfa8e025f27d14f6ef22ef280
Bapiya <bapiya1@gmail.com>
2024-09-29 12:05:06 +0530

    fix: 1.0.5

--- front/src/Home/index.jsx
+++ front/src/Home/index.jsx
@@ -1,14 +1,23 @@
-import React, { useContext } from "react";
+import { useContext, useRef } from "react";
 import { FDfrontContext } from "../App";
 import RestArr from "../RestArr";
 import close from "../assets/close.svg";
+import leftArrow from "../assets/left-arrow.svg";
 
 const Home = () => {
   const { rtypes, setRestfilter, restfilter } = useContext(FDfrontContext);
+  const ref = useRef();
 
   const handleClick = (e) => {
     const key = e.target.dataset.ind;
     setRestfilter({ name: key, type: "rtype" });
+  };
+
+  const scrollLeft = () => {
+    ref.current.scrollLeft -= 150;
+  };
+  const scrollRight = () => {
+    ref.current.scrollLeft += 150;
   };
 
   return (
@@ -48,32 +57,73 @@ const Home = () => {
           <p style={{ fontSize: "1.6rem", margin: "5px 40px" }}>
             Whats On Your Mind
           </p>
-          <div style={{ display: "flex", width: "90%", maxWidth: 1100 }}>
-            {rtypes.map((el, i) => {
-              return (
-                <div
-                  key={i}
-                  data-ind={el.name}
-                  onClick={handleClick}
-                  className="rtypel"
-                >
-                  <img
-                    src={el.img}
-                    alt=""
+          <div
+            style={{
+              width: "90%",
+              maxWidth: 1100,
+              position: "relative",
+            }}
+          >
+            <div className="scroll-btns">
+              <img
+                src={leftArrow}
+                onClick={scrollLeft}
+                style={{
+                  borderRadius: "50%",
+                  position: "absolute",
+                  top: "50%",
+                  left: 0,
+                  padding: 5,
+                  cursor: "pointer",
+                  translate: "-50% -100%",
+                  backgroundColor: "white",
+                  zIndex: 1,
+                }}
+              />
+              <img
+                src={leftArrow}
+                onClick={scrollRight}
+                style={{
+                  borderRadius: "50%",
+                  position: "absolute",
+                  top: "50%",
+                  right: 0,
+                  padding: 5,
+                  cursor: "pointer",
+                  translate: "50% -100%",
+                  rotate: "180deg",
+                  zIndex: 1,
+                  backgroundColor: "white",
+                }}
+              />
+            </div>
+            <div ref={ref} style={{ display: "flex", overflowX: "hidden" }}>
+              {rtypes.map((el, i) => {
+                return (
+                  <div
+                    key={i}
                     data-ind={el.name}
-                    loading="lazy"
-                    style={{
-                      height: 130,
-                      width: 130,
-                      borderRadius: "50%",
-                      objectFit: "cover",
-                      margin: "10px 0",
-                    }}
-                  />
-                  <p data-ind={el.name}>{el.name}</p>
-                </div>
-              );
-            })}
+                    onClick={handleClick}
+                    className="rtypel"
+                  >
+                    <img
+                      src={el.img}
+                      alt=""
+                      data-ind={el.name}
+                      loading="lazy"
+                      style={{
+                        height: 130,
+                        width: 130,
+                        borderRadius: "50%",
+                        objectFit: "cover",
+                        margin: "10px 0",
+                      }}
+                    />
+                    <p data-ind={el.name}>{el.name}</p>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       )}
